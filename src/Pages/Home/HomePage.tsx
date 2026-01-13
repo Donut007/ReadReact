595c17bb261bf9594fe17faaef313cc23c20266f
Donut007 <donut_zero4@hotmail.com>
2026-01-13 23:38:07 +0700

    Update

--- src/Pages/Home/HomePage.tsx
+++ src/Pages/Home/HomePage.tsx
@@ -1,8 +1,40 @@
 
+import { supabase } from '../../supabaseClient'
+import { useState, useEffect } from 'react'
+
+type User = {
+  ID: number
+  Name: string
+}
 
 function HomePage() {
+  const [users, setUsers] = useState<User[]>([])
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const { data, error } = await supabase
+        .from('TB_M_User')
+        .select('*')
+
+      if (error) {
+        console.error(error)
+      } else {
+        setUsers(data)
+      }
+    }
+
+    fetchUsers()
+  }, [])
+  
   return (
-    <div>Home</div>
+    <div>Home
+      <div>
+        <h1>Supabase Users</h1>
+        {users.map(u => (
+          <div key={u.ID}>{u.Name}</div>
+        ))}
+      </div>
+    </div>
   )
 }
 

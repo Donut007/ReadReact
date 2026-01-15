8f557da71b4d294868f084008bf104550d81f1ce
Donut007 <donut_zero4@hotmail.com>
2026-01-15 23:34:22 +0700

    Update

--- src/components/LoginModal.tsx
+++ src/components/LoginModal.tsx
@@ -1,9 +1,5 @@
-import { Modal, Box, TextField } from "@mui/material";
-import { signIn } from "../Services/auth.service";
-import { useState } from "react";
-import { Auth } from "../Lib/supabaseClient";
-import { ThemeSupa } from "@supabase/auth-ui-shared";
-import { supabase } from "../supabaseClient";
+import { Box, Button, Modal, TextField } from "@mui/material";
+import { useForm } from "react-hook-form";
 
 interface LoginModalProps {
     open: boolean;
@@ -20,46 +16,29 @@ const style = {
     boxShadow: 24,
     p: 3,
     borderRadius: 2,
+    backgroundColor: "black",
 };
 
 export default function LoginModal({
     open,
     onClose
 }: LoginModalProps) {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [errors, setErrors] = useState<{ email?: string; password?: string }>({});
+    const { register, handleSubmit, formState: { errors } } = useForm();
 
-    setErrors({}); // Clear previous errors
     return (
         <Modal open={open} onClose={onClose}>
-            {/* <Box sx={style}>
+            <Box sx={style}>
+                <form onSubmit={handleSubmit(console.log)}>
+                    <TextField
+                        label="Email"
+                        {...register("email", { required: "Email required" })}
+                        error={!!errors.email}
+                        helperText={errors.email?.message as string}
+                    />
+                    <Button type="submit">Submit</Button>
+                </form>
+            </Box>
 
-                <TextField
-                    label="Username or Email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    error={!!errors.email}
-                    helperText={errors.email}
-                    fullWidth
-                />
-
-                <TextField
-                    label="Password"
-                    type="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    error={!!errors.password}
-                    helperText={errors.password}
-                    fullWidth
-                />
-
-            </Box> */}
-            <Auth
-                supabaseClient={supabase}
-                appearance={{ theme: ThemeSupa }}
-                providers={[]}
-            />
         </Modal>
     );
 }
